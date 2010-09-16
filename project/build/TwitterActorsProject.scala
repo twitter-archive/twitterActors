9ba65a021b30858d3d881ed187d15844a101f00d
Attila Szegedi <szegedia@gmail.com>
2010-09-16 15:10:56 +0200

    Update to standard project 0.7.1

--- project/build/TwitterActorsProject.scala
+++ project/build/TwitterActorsProject.scala
@@ -1,8 +1,10 @@
 import sbt._
 import Process._
-import com.twitter.sbt.{SubversionRepository, StandardProject}
+import com.twitter.sbt.{SubversionPublisher, StandardProject}
 
 
-class TwitterActorsProject(info: ProjectInfo) extends StandardProject(info) with SubversionRepository {
+class TwitterActorsProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
   val specs     = "org.scala-tools.testing" % "specs" % "1.6.2.1"
+
+  override def subversionRepository = Some("http://svn.local.twitter.com/maven-public")
 }
